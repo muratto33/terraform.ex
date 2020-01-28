8c66e6f1a73bb45a323a043340b5732af4133d05
muratto33 <muratkilic33@yahoo.com>
2020-01-28 09:34:58 -0600

    ec2

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+esource "aws_instance" "web" { 
+  ami = "ami-00068cd7555f543d5" 
+  instance_type = "t2.micro" 
+  associate_public_ip_address = "true" 
+  key_name = aws_key_pair.deployer.key_name 
+  tags = { 
+    Name = "HelloWorld" 
+  } 
+} 
