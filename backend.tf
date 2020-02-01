bfdc895d0c8f83a9798c936439122e5f4297abd9
muratto33 <muratkilic33@yahoo.com>
2020-02-01 13:14:22 -0600

    add

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-us-murat"
-    key    = "aws/iam/iam_state"
+    bucket = "backend-state-murat"
+    key    = "infrastructure"
     region = "us-east-1"
   }
 }
