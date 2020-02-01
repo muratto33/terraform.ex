120f06fbd3708b640f027d1cc5830826e2f571f2
muratto33 <muratkilic33@yahoo.com>
2020-02-01 14:45:04 -0600

    add userneame

--- output.tf
+++ output.tf
@@ -1,11 +1,32 @@
 output "bucket_name" {
-    value = "${aws_s3_bucket.b.bucket}"
+    value = aws_s3_bucket.b.bucket 
 }
 
+
 output "sec_group" {
-    value = "${aws_security_group.allow_ssh.name}"
+    value = aws_security_group.allow_ssh.name 
+
 }
 
 output "key_name" {
-    value = "${aws_key_pair.deployer.key_name}"
+    value = aws_key_pair.deployer1.key_name 
+
 }
+
+output "wordpress" {
+    value = aws_route53_record.www.name 
+  
+
+}
+
+output "wordpress_username" {
+    value = "some_username"
+}
+
+    output "wordpress_password" {
+    value = "some_password"
+    }
+
+     output "IPs" {
+    value = aws_instance.web.*.public_ip
+     }
