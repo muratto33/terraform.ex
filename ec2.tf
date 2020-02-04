a9697b29d000e4a2094c0fbaa877e62b14d5ca5b
muratto33 <muratkilic33@yahoo.com>
2020-02-04 07:34:26 -0600

    add imprt argument

--- ec2.tf
+++ ec2.tf
@@ -15,9 +15,7 @@ resource "aws_instance" "web" {
     Name = "HelloWorld${count.index +1}" 
   } 
 } 
-
-resource "aws_instance" "imported" {
-  key_name = aws_key_pair.deployer.key_name 
-  security_groups = ["allow_ssh"]
-  # (resource arguments)
-}
+ resource "aws_instance" "imported" {
+  key_name   = aws_key_pair.deployer.key_name 
+  security_groups    = ["allow_ssh"]
+} 
