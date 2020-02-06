8a27cec83b5e52855803dea11a23e7d373ffcebb
muratto33 <muratkilic33@yahoo.com>
2020-02-06 08:54:04 -0600

    add variable

--- TEST/ec2.tf
+++ TEST/ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" { 
-  ami           = "AMI_ID_HERE" 
-  instance_type = "t2.micro" 
+  ami           = var.ami
+  instance_type = var.instance_type 
+  associate_public_ip_address = var.associate_public_ip_address
+  count = var.count_instance
  
 tags = { 
     Name = "HelloWorld" 
---
+++ TEST/variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "associate_public_ip_address" {}
+variable "count_instance" {
+  
+}
+
+
+
+
+
+
