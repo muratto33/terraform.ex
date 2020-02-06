31113ee25c075a3d247c62a58ca059d1f2ad1de5
muratto33 <muratkilic33@yahoo.com>
2020-02-06 09:32:50 -0600

    add key pair

--- TEST/ec2.tf
+++ TEST/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type 
   associate_public_ip_address = var.associate_public_ip_address
   count = var.count_instance
+  key_name = aws_key_pair.deployer.key_name 
   security_groups = ["allow_ssh"]
  
 tags = { 
