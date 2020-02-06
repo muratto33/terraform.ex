e422cd4026373996b74a903822c6296cf9e112d4
muratto33 <muratkilic33@yahoo.com>
2020-02-06 09:20:12 -0600

    attach sg

--- TEST/ec2.tf
+++ TEST/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type 
   associate_public_ip_address = var.associate_public_ip_address
   count = var.count_instance
+  security_group = ["allow_ssh"]
  
 tags = { 
     Name = "HelloWorld" 
--- TEST/variables.tf
+++ TEST/variables.tf
@@ -1,9 +1,9 @@
 variable "ami" {}
 variable "instance_type" {}
 variable "associate_public_ip_address" {}
-variable "count_instance" {
-  
-}
+variable "count_instance" {}
+
+
 
 
 
