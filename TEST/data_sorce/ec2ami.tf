db9366ec82c67e846b02d7b416808899431be606
muratto33 <muratkilic33@yahoo.com>
2020-02-08 14:28:02 -0600

    add data source

---
+++ TEST/data_sorce/ec2ami.tf
@@ -0,0 +1,28 @@
+# Go to region us-east-2
+provider "aws" {
+region = "us-east-2"
+}
+
+# Search for ubuntu latest with the owner
+data "aws_ami" "ubuntu" {
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+        }
+    most_recent = true
+    owners = ["099720109477"]
+}
+
+# Show the AMI id
+output "ami" {
+    value = "${data.aws_ami.ubuntu.id}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
